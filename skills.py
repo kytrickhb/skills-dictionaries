0a5c74e0231fede9a93d1cc20b65708ea437f90c
Kayla Smith <kytrick@gmail.com>
2015-10-04 20:36:24 -0700

    initial commit

---
+++ skills.py
@@ -0,0 +1,386 @@
+
+import itertools
+# To work on the advanced problems, set to True
+ADVANCED = True
+
+
+def count_unique(input_string):
+    """Count unique words in a string.
+
+    This function should take a single string and return a dictionary
+    that has all of the distinct words as keys, and the number of times
+    that word appears in the string as values.
+
+
+    For example:
+
+        >>> print_dict(count_unique("each word appears once"))
+        {'appears': 1, 'each': 1, 'once': 1, 'word': 1}
+
+    Words that appear more than once should be counted each time:
+
+        >>> print_dict(count_unique("rose is a rose is a rose"))
+        {'a': 2, 'is': 2, 'rose': 3}
+
+    It's fine to consider punctuation part of a word (e.g., a comma
+    at the end of a word can be counted as part of that word) and
+    to consider differently-capitalized words as different:
+
+        >>> print_dict(count_unique("Porcupine see, porcupine do."))
+        {'Porcupine': 1, 'do.': 1, 'porcupine': 1, 'see,': 1}
+
+    """
+    D = {}   
+    for w in input_string.split():
+      if w not in D:
+        D[w] = 1
+      else:
+        D[w] += 1 
+    return D
+
+
+def find_common_items(list1, list2):
+    """Produce the set of common items in two lists.
+
+    Given two lists, return a list of the common items shared between
+    the lists.
+
+    IMPORTANT: you may not use 'if ___ in ___' or the method 'index'.
+
+
+    For example:
+
+        >>> sorted(find_common_items([1, 2, 3, 4], [1, 2]))
+        [1, 2]
+
+    If an item appears more than once in at least one list and is present
+    in both lists, return it each time:
+
+        >>> sorted(find_common_items([1, 2, 3, 4], [1, 1, 2, 2]))
+        [1, 1, 2, 2]
+
+    (And the order of which has the multiples shouldn't matter, either):
+
+        >>> sorted(find_common_items([1, 1, 2, 2], [1, 2, 3, 4]))
+        [1, 1, 2, 2]
+
+    """
+    newlist = []
+    for i in list1:
+        for j in list2:
+            if i == j:
+                newlist.append(i)
+
+    return newlist
+
+
+def find_unique_common_items(list1, list2):
+    """Produce the set of *unique* common items in two lists.
+
+    Given two lists, return a list of the *unique* common items shared between
+    the lists.
+
+    IMPORTANT: you may not not 'if ___ in ___' or the method 'index'.
+
+
+    Just like `find_common_items`, this should find [1, 2]:
+
+        >>> sorted(find_unique_common_items([1, 2, 3, 4], [1, 2]))
+        [1, 2]
+
+    However, now we only want unique items, so for these lists, don't show
+    more than 1 or 2 once:
+
+        >>> sorted(find_unique_common_items([1, 2, 3, 4], [1, 1, 2, 2]))
+        [1, 2]
+
+    """
+
+    return list(set(list1) & set(list2))
+
+
+def get_sum_zero_pairs(input_list):
+    """Given a list of numbers, return list of x,y number pair lists where x + y == 0.
+
+    Given a list of numbers, add up each individual pair of numbers.
+    Return a list of each pair of numbers that adds up to 0.
+
+
+    For example:
+
+        >>> sort_pairs( get_sum_zero_pairs([1, 2, 3, -2, -1]) )
+        [[-2, 2], [-1, 1]]
+
+        >>> sort_pairs( get_sum_zero_pairs([3, -3, 2, 1, -2, -1]) )
+        [[-3, 3], [-2, 2], [-1, 1]]
+
+    This should always be a unique list, even if there are
+    duplicates in the input list:
+
+        >>> sort_pairs( get_sum_zero_pairs([1, 2, 3, -2, -1, 1, 1]) )
+        [[-2, 2], [-1, 1]]
+
+    Of course, if there are one or more zeros to pair together,
+    that's fine, too (even a single zero can pair with itself):
+
+        >>> sort_pairs( get_sum_zero_pairs([1, 2, 3, -2, -1, 1, 1, 0]) )
+        [[-2, 2], [-1, 1], [0, 0]]
+
+    """
+
+    # First I'm generating a list of lists of combinations of the input list.
+    # Next I'm filtering with a lambda that unpacks the tuple and adds the 
+    # unpacked values together.  
+    # Finally filtering on whether that sum is equal to 0
+
+    return filter(lambda (x,y): x + y == 0, 
+                  [list(i) for i in itertools.combinations(set(input_list), 2)])
+
+
+def remove_duplicates(words):
+    """Given a list of words, return the list with duplicates removed.
+
+    Do this without using a Python set.
+
+    For example:
+
+        >>> sorted(remove_duplicates(
+        ...     ["rose", "is", "a", "rose", "is", "a", "rose"]))
+        ['a', 'is', 'rose']
+
+    You should treat differently-capitalized words as different:
+
+        >>> sorted(remove_duplicates(
+        ...     ["Rose", "is", "a", "rose", "is", "a", "rose"]))
+        ['Rose', 'a', 'is', 'rose']
+
+    """
+    L = []
+    for w in words:
+        if w not in L:
+            L.append(w)
+
+    return L
+
+
+def encode(phrase):
+    """Given a phrase, return the encoded string.
+
+    Replace all "e" characters with "p",
+    replace "a" characters with "d", replace "t" characters with "o",
+    and "i" characters with "u".
+
+    For example:
+
+        >>> encode("You are a beautiful, talented, brilliant, powerful musk ox.")
+        'You drp d bpduouful, odlpnopd, brulludno, powprful musk ox.'
+    """
+    return phrase.replace("e","p").replace("a","d").replace("t","o").replace("i","u")
+    
+
+
+def sort_by_word_length(words):
+    """Given list of words, return list of ascending [(len, [words])].
+
+    Given a list of words, return a list of tuples, ordered by word-length.
+    Each tuple should have two items--the length of the words for that
+    word-length, and the list of words of that word length.
+
+    For example:
+
+        >>> sort_by_word_length(["ok", "an", "apple", "a", "day"])
+        [(1, ['a']), (2, ['ok', 'an']), (3, ['day']), (5, ['apple'])]
+
+    """
+    D = {}
+    for w in words: #build D where keys are len and values are words of that len
+        if len(w) not in D:
+            D[len(w)] = [w]
+        else:
+            D[len(w)].append(w)
+
+    return sorted(D.items())
+
+
+def get_pirate_talk(phrase):
+    """Translate phrase to pirate talk.
+
+    Given a phrase, translate each word to the Pirate-speak equivalent.
+    Words that cannot be translated into Pirate-speak should pass through
+    unchanged. Return the resulting sentence.
+
+    Here's a table of English to Pirate translations:
+
+    English     Pirate
+    ----------  ----------------
+    sir         matey
+    hotel       fleabag inn
+    student     swabbie
+    boy         matey
+    madam       proud beauty
+    professor   foul blaggart
+    restaurant  galley
+    your        yer
+    excuse      arr
+    students    swabbies
+    are         be
+    lawyer      foul blaggart
+    the         th'
+    restroom    head
+    my          me
+    hello       avast
+    is          be
+    man         matey
+
+    For example:
+
+        >>> get_pirate_talk("my student is not a man")
+        'me swabbie be not a matey'
+
+    You should treat words with punctuation as if they were different
+    words:
+
+        >>> get_pirate_talk("my student is not a man!")
+        'me swabbie be not a man!'
+
+    """
+ 
+    T = {
+    'are': 'be',
+    'boy': 'matey',
+    'excuse': 'arr',
+    'hello': 'avast',
+    'hotel': 'fleabag inn',
+    'is': 'be',
+    'lawyer': 'foul blaggart',
+    'madam': 'proud beauty',
+    'man': 'matey',
+    'my': 'me',
+    'professor': 'foul blaggart',
+    'restaurant': 'galley',
+    'restroom': 'head',
+    'sir': 'matey',
+    'student': 'swabbie',
+    'students': 'swabbies',
+    'the': "th'",
+    'your': 'yer'}
+
+    pirate_list = []
+    
+    for w in phrase.split():
+        if w in T:
+            pirate_list.append(T[w])
+        else:
+            pirate_list.append(w)
+
+    return " ".join(pirate_list)
+
+
+    # Not sure if we were supposed to process the translations into a dictionary
+    # as part of the assignment.  If so here it is.
+    # T = {}
+    # text = open("translations.txt").readlines()
+    # for t in text:
+    #     s = t.strip().split(" ", 1)
+    #     T[s[0]]= s[1].strip()
+    #     print T
+
+# End of skills. See below for advanced problems.
+# To work on them, set ADVANCED=True at the top of this file.
+############################################################################
+
+
+def adv_get_top_letter(input_string):
+    """Given an input string, return a list of letter(s) which appear(s) the most the input string.
+
+    If there is a tie, the order of the letters in the returned
+    list should be alphabetical.
+
+    For example:
+        >>> adv_get_top_letter("The rain in spain stays mainly in the plain.")
+        ['i', 'n']
+
+    If there is not a tie, simply return a list with one item.
+
+    For example:
+        >>> adv_get_top_letter("Shake it off, shake it off. Shake it off, Shake it off.")
+        ['f']
+
+    Spaces do not count as letters.
+
+    """
+    tl = "" #initializing the top letter(s) string
+    D = {}
+
+    # build the frequency dictionary
+    for i in input_string.replace(" ",""):
+        if i not in D:
+            D[i] = 1
+        else:
+            D[i] += 1
+
+    # retrieve the letter(s) which appear the max amount of time(s)
+    for x in D:
+        if D[x] == max(D.values()):
+            tl += x
+
+    # print the top letters in sorted order
+    return sorted(tl)
+
+def adv_alpha_sort_by_word_length(words):
+    """Given a list of words, return a list of tuples, ordered by word-length.
+
+    Each tuple should have two items--a number that is a word-length,
+    and the list of words of that word length. In addition to ordering
+    the list by word length, order each sub-list of words alphabetically.
+    Now try doing it with only one call to .sort() or sorted(). What does the
+    optional "key" argument for .sort() and sorted() do?
+
+    For example:
+
+        >>> adv_alpha_sort_by_word_length(["ok", "an", "apple", "a", "day"])
+        [(1, ['a']), (2, ['an', 'ok']), (3, ['day']), (5, ['apple'])]
+
+    """
+    D = {}
+    # construct dictionary with lenth as key, and words in a list as values
+    for word in words:
+        if len(word) not in D:
+            D[len(word)] = [word]
+        else:
+            D[len(word)].append(word)
+
+    # build the output up in the way specified
+    asbwl = []
+    for i in sorted(D.keys()):
+        asbwl.append((i, sorted(D[i])))
+    return asbwl
+
+
+##############################################################################
+# You can ignore everything below this.
+
+def print_dict(d):
+    # This method is just used to print dictionaries in key-alphabetical
+    # order, and is only used for our documentation tests. You can ignore it.
+    if isinstance(d, dict):
+        print "{" + ", ".join("%r: %r" % (k, d[k]) for k in sorted(d)) + "}"
+    else:
+        print d
+
+
+def sort_pairs(l):
+    # Print sorted list of pairs where the pairs are sorted. This is used only
+    # for documentation tests. You can ignore it.
+    return sorted(sorted(pair) for pair in l)
+
+if __name__ == "__main__":
+    print
+    import doctest
+    for k, v in globals().items():
+        if k[0].isalpha():
+            if k.startswith('adv_') and not ADVANCED:
+                continue
+            a = doctest.run_docstring_examples(v, globals(), name=k)
+    print "** END OF TEST OUTPUT"
+    print
